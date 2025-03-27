189d9f398d85c6c75655c4d1518fbed314ba3918
Ahmed Qarooni <aaq2002@gmail.com>
2025-03-27 04:25:07 +0300

    no tasks yet msg

--- src/components/PersonalTaskList.jsx
+++ src/components/PersonalTaskList.jsx
@@ -18,13 +18,15 @@ export const PersonalTaskList = ({ tasks, addTask, editTask, deleteTask}) => {
     <>
       <div className="h-3/4 bg-blue-400/40 rounded-3xl border-2 border-blue-400 p-4 overflow-y-auto [&::-webkit-scrollbar]:w-0
       ">
-        <h2 className="text-2xl  font-bold mb-2 no-scroll">Personal</h2>
+        <h2 className="text-2xl  font-bold mb-2 ">Personal</h2>
         <div className="flex flex-col gap-2 pb-2 
       "> {
+        taskList.length != 0 ? 
         taskList.map((task) => (
 
           <TaskCard key={task.taskId} task={task} onDelete={deleteTask} />
-        ))
+        )) :
+        <h2 className="text-xl  font-bold mb-2">No Tasks Yet!</h2>
       }
           {/* <TaskCard task={task} />
           <TaskCard task={task} />
