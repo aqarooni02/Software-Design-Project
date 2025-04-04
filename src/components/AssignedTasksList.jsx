deca7367e8330e5ffd9c42ffed05987bbe39ace9
ZainMayoof <zainaqeelmayoof@gmail.com>
2025-04-04 20:41:41 +0300

    local storage done for assigning tasks, appears both on parent and child's list

--- src/components/AssignedTasksList.jsx
+++ src/components/AssignedTasksList.jsx
@@ -1,35 +1,40 @@
-export const AssignedTaskList = ({ childType }) => {
-    return (
-        <>
-            <h2
-                className={
-                    childType === "orange"
-                        ? "text-2xl font-bold mb-2 text-orange-700"
-                        : "text-2xl font-bold mb-2 text-pink-700"
-                }
-            >
-                Assigned by Parent
-            </h2>
-            <div
-                className={
-                    childType === "orange"
-                        ? "h-3/4 bg-orange-400/40 rounded-3xl border-2 border-orange-400 p-4 overflow-y-auto"
-                        : "h-3/4 bg-pink-400/40 rounded-3xl border-2 border-pink-400 p-4 overflow-y-auto"
-                }
-            >
+import { useEffect, useState } from "react";
+import { TaskCard } from "./TaskCard";
 
-                <div className="flex flex-col gap-2 pb-2">
-                    <p
-                        className={
-                            childType === "orange"
-                                ? "text-orange-700 text-lg"
-                                : "text-pink-700 text-lg"
-                        }
-                    >
-                        No tasks assigned yet!
-                    </p>
-                </div>
-            </div>
-        </>
-    )
-}
+export const AssignedTaskList = ({ childId, childType }) => {
+  const localStorageKey = `child_${childId}`;
+  const [assignedTasks, setAssignedTasks] = useState([]);
+
+  useEffect(() => {
+    const storedChild = JSON.parse(localStorage.getItem(localStorageKey));
+    const newTasks = storedChild && storedChild.assignedTasks ? storedChild.assignedTasks : [];
+    setAssignedTasks(newTasks);
+  }, [childId, localStorageKey]);
+
+  const headingClasses =
+    childType === "orange"
+      ? "text-2xl font-bold mb-2 text-orange-700"
+      : "text-2xl font-bold mb-2 text-pink-700";
+
+  const boxClasses =
+    childType === "orange"
+      ? "h-3/4 bg-orange-400/40 rounded-3xl border-2 border-orange-400 p-4 overflow-y-auto"
+      : "h-3/4 bg-pink-400/40 rounded-3xl border-2 border-pink-400 p-4 overflow-y-auto";
+
+  return (
+    <>
+      <h2 className={headingClasses}>Assigned by Parent</h2>
+      <div className={boxClasses}>
+        {assignedTasks.length !== 0 ? (
+          <div className="flex flex-col gap-2 pb-2">
+            {assignedTasks.map((task) => (
+              <TaskCard key={task.taskId} task={task} theme={childType} />
+            ))}
+          </div>
+        ) : (
+          <h2 className="text-xl font-bold mb-2">No tasks assigned yet!</h2>
+        )}
+      </div>
+    </>
+  );
+};
